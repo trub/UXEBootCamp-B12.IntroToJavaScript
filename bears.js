57b13e03c4d76f19a2e2df88f02d2dc8fc965e77
matthew weintrub <heytrub@gmail.com>
2014-03-24 17:17:33 -0700

    this adds javascript by trub + koch

---
+++ bears.js
@@ -0,0 +1,28 @@
+var papa_bear = "joe ";
+var mama_bear = "joe anne ";
+var jr_bear = "joe jr ";
+var daughter_bear = "pear has lots of hair ";
+
+var bears = [papa_bear, mama_bear, jr_bear, daughter_bear];
+
+alert("Warning! There are four bears near your home!");
+
+var input = prompt("Are the bears hungry or fed?");
+
+if (input === "hungry"){
+      
+        alert(bears +  "will eat you alive!");
+ 
+	} else if (input === "fed") {
+
+	alert(bears + " are sleepy");
+    
+	} else {
+
+		alert("stooopid");
+
+}
+
+
+
+
